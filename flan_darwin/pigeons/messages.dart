52a5ea6e35c1bd958ee939b55466f1b80dd7e511
Vijay Prakash <prakash.vijay@gmail.com>
2025-01-09 21:35:50 -0500

    Start implementing flan_darwin

---
+++ flan_darwin/pigeons/messages.dart
@@ -0,0 +1,34 @@
+import 'package:pigeon/pigeon.dart';
+
+@ConfigurePigeon(PigeonOptions(
+  dartOut: 'lib/src/messages.g.dart',
+  swiftOut: 'darwin/flan_darwin/Sources/flan_darwin/messages.g.swift',
+))
+
+/// The purpose of this Pigeon-defined API is basically to ensure type safety
+/// of the primitive types when going between Dart and Swift and to provide
+/// a slightly cleaner interface. As such, the API uses primitive types in
+/// places of some real Dart types.
+@HostApi()
+abstract class FlanDarwinApi {
+  @async
+  Map<String, dynamic> getNotificationSettingsAsync();
+
+  @async
+  void requestAuthorizationAsync(
+    List<String> options,
+  );
+
+  @async
+  void scheduleNotificationAsync(
+    String id,
+    String targetTimestamp,
+    Map<String, dynamic> content,
+    bool repeats,
+  );
+
+  void cancelNotifications(List<String> ids);
+
+  @async
+  List<Map<String, dynamic>> getScheduledNotificationsAsync();
+}

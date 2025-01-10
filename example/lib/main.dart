f52e85212864c73aea89ff4ecaa6b231b4768772
Vijay Prakash <prakash.vijay@gmail.com>
2025-01-10 11:07:36 -0500

    Re-export some things that should be accessible from the flan library

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,27 +1,71 @@
 import 'package:flan/flan.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:ksuid/ksuid.dart';
 
 void main() {
-  runApp(const MainApp());
+  GetIt.instance.registerSingleton<FlanPlatformApi>(FlanPlatformApi());
+  runApp(const FlanExampleApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class FlanExampleApp extends StatefulWidget {
+  const FlanExampleApp({super.key});
 
-  static final FlanPlatformApi _flan = FlanPlatformApi();
+  @override
+  State<FlanExampleApp> createState() => _FlanExampleAppState();
+}
+
+class _FlanExampleAppState extends State<FlanExampleApp> {
+  static final FlanPlatformApi _flan = GetIt.instance<FlanPlatformApi>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
         body: Center(
-          child: Row(
+          child: Column(
             spacing: 8,
             children: [
-              TextButton(
-                  child: Text('Print notification settings'),
-                  onPressed: () async =>
-                      _flan.getNotificationSettingsAsync().then(print)),
+              OutlinedButton(
+                child: Text('Request notification authorization'),
+                onPressed: () async {
+                  await _flan.requestAuthorizationAsync(
+                    [
+                      NotificationAuthorizationOptions.alert,
+                    ],
+                  );
+                },
+              ),
+              OutlinedButton(
+                child: Text('Schedule notification in 10 seconds'),
+                onPressed: () async {
+                  DateTime target = DateTime.now().add(Duration(seconds: 10));
+                  NotificationContent content = NotificationContent(
+                    title: 'Example notification from Flan',
+                    body: 'Hi from Flan!',
+                  );
+
+                  await _flan.scheduleNotificationAsync(
+                    KSUID.generate().asString,
+                    target,
+                    content,
+                    repeats: false,
+                  );
+                },
+              ),
+              OutlinedButton(
+                  child: Text('Get notification settings'),
+                  onPressed: () async {
+                    print(await _flan.getNotificationSettingsAsync());
+                  })
             ],
           ),
         ),
--- flan/lib/flan.dart
+++ flan/lib/flan.dart
@@ -1,1 +1,3 @@
+export 'package:flan_platform_interface/flan_platform_interface.dart'
+    show FlanApi, NotificationAuthorizationOptions, NotificationContent;
 export 'src/flan_platform_api.dart';
